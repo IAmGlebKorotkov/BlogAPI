d15299d7ec0584bc8cd278d5b1722821640630cf
IAmGlebKorotkov <korotkovgleb200@gmail.com>
2024-12-08 06:37:34 +0700

    Add Tag Controller + Filter

---
+++ BlogAPI/Migrations/20241207231447_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "fias");
+
+            migrationBuilder.CreateTable(
+                name: "tag",
+                schema: "fias",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    create_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tag", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tag",
+                schema: "fias");
+        }
+    }
+}
